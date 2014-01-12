43a2ae558f47fa088ce3145d06608ddc55b6e7c8
Jakub <Jakub@aku-nb>
2014-01-12 17:43:55 +0000

    Commenting

--- src/cz/cuni/mff/bc/server/ActiveClient.java
+++ src/cz/cuni/mff/bc/server/ActiveClient.java
@@ -8,6 +8,7 @@ import java.util.Timer;
 import org.cojen.dirmi.Session;
 
 /**
+ * Stores information about active client connection
  *
  * @author Jakub Hava
  */
@@ -20,55 +21,109 @@ public class ActiveClient {
     private int memoryLimit;
     private int coresLimit;
 
-    public String getClientName() {
-        return clientName;
-    }
-
+    /**
+     * Constructor
+     *
+     * @param clientName client's name
+     * @param session client's session
+     */
     public ActiveClient(String clientName, Session session) {
         this.clientName = clientName;
         this.session = session;
     }
 
+    /**
+     *
+     * @return client's name
+     */
+    public String getClientName() {
+        return clientName;
+    }
+
+    /**
+     *
+     * @param clientName client's name
+     */
     public void setClientName(String clientName) {
         this.clientName = clientName;
     }
 
+    /**
+     *
+     * @return timer
+     */
     public Timer getTimer() {
         return timer;
     }
 
+    /**
+     *
+     * @param timer timer
+     */
     public void setTimer(Timer timer) {
         this.timer = timer;
     }
 
+    /**
+     *
+     * @return timeout
+     */
     public Boolean getTimeout() {
         return timeout;
     }
 
+    /**
+     *
+     * @param timeout timeout
+     */
     public void setTimeout(Boolean timeout) {
         this.timeout = timeout;
     }
 
+    /**
+     *
+     * @return client's session
+     */
     public Session getSession() {
         return session;
     }
 
+    /**
+     *
+     * @param session client's session
+     */
     public void setSession(Session session) {
         this.session = session;
     }
 
+    /**
+     *
+     * @return memory limit
+     */
     public int getMemoryLimit() {
         return memoryLimit;
     }
 
+    /**
+     *
+     * @param memoryLimit memory limit
+     */
     public void setMemoryLimit(int memoryLimit) {
         this.memoryLimit = memoryLimit;
     }
 
+    /**
+     *
+     * @return cores limit
+     */
     public int getCoresLimit() {
         return coresLimit;
     }
 
+    /**
+     *
+     * @param coresLimit cores limit
+     */
     public void setCoresLimit(int coresLimit) {
         this.coresLimit = coresLimit;
     }
