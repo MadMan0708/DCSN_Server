90de23649b259fd74b698a42f8ceedf50191a54e
Jakub <Jakub@192.168.1.202>
2014-06-08 13:12:15 +0200

    comment update

--- src/cz/cuni/mff/bc/server/strategies/MaxThroughputStrategy.java
+++ src/cz/cuni/mff/bc/server/strategies/MaxThroughputStrategy.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Implementation of Maximal Throughput strategy
